36dd34220d781fb58751a20db0a384223daf0760
Jessica <jvs.luk@gmail.com>
2018-11-29 12:49:13 +0100

    Problem 3
    First bit of code added

---
+++ 3- Largest Prime Factor/3- Largest Prime Factor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3__Largest_Prime_Factor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////////////////////////////////
+            /// The prime factors of 13195 are 5, 7, 13 and 29.                ///
+            ///                                                                ///
+            /// What is the largest prime factor of the number 600851475143?   ///
+            ///                                                                ///
+            /// Written by Jessica Luk-Maduro on 29-11-2018                    ///
+            //////////////////////////////////////////////////////////////////////
+
+            long number = 600851475143;
+            long primeInNum = 0;
+
+
+
+        }
+    }
+}
