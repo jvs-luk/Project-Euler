f0807a044d3d616b2df0ab41f587440cb1c033c2
Jessica <jvs.luk@gmail.com>
2018-12-03 12:29:35 +0100

    Continued work on Problem 3
    Getting incorrect answer out of code, will need to look over/rubber duckie it later

--- 3- Largest Prime Factor/3- Largest Prime Factor/Program.cs
+++ 3- Largest Prime Factor/3- Largest Prime Factor/Program.cs
@@ -15,14 +15,38 @@ namespace _3__Largest_Prime_Factor
             ///                                                                ///
             /// What is the largest prime factor of the number 600851475143?   ///
             ///                                                                ///
-            /// Written by Jessica Luk-Maduro on 29-11-2018                    ///
+            /// Written by Jessica Luk-Maduro on 03-12-2018                    ///
             //////////////////////////////////////////////////////////////////////
 
-            long number = 600851475143;
-            long primeInNum = 0;
+            const long number = 600851475143;
+            double currentNum = Convert.ToInt32(Math.Sqrt(600851475143) + 1);     // we only need to check numbers up 
+            long largestFact = 0;                                                 // to the square root to find primes
+            int newFact = 3;                  // 3 is the smallest prime number
 
+            while (currentNum > 1)
+            {
+                // first check if number is divisible by 2; any prime factor should not be
+                if (newFact%2 == 0 || currentNum%newFact > 0)
+                {
+                    newFact++;
+                }
 
+                // get factors of currentNum
+                else if(currentNum%newFact == 0)
+                {
+                    currentNum = currentNum/newFact;  
+                }
 
+                // find largestFact
+                if (largestFact == 0 || newFact > largestFact)
+                {
+                    largestFact = newFact;
+                    Console.WriteLine(largestFact);
+                }
+            }
+
+            Console.WriteLine("The largest prime factor of " + number + " is " + largestFact + ".");
+            Console.ReadLine();
         }
     }
 }
