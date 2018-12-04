ab1c9283752dcfb98d906293dd29e38252d0d491
Jessica <jvs.luk@gmail.com>
2018-12-04 12:47:31 +0100

    Revert "Merge branch 'master' into Problem3"
    This reverts commit 62db7a432bf93eb2d620f0b2ae9d76ce80061f6a.

--- 3- Largest Prime Factor/3- Largest Prime Factor/Program.cs
+++ 3- Largest Prime Factor/3- Largest Prime Factor/Program.cs
@@ -10,35 +10,19 @@ namespace _3__Largest_Prime_Factor
     {
         static void Main(string[] args)
         {
-            const long number = 600851475143;
-            double currentNum = 
-                Convert.ToInt32(Math.Sqrt(600851475143) + 1);     
-            long largestFact = 3;                                                 
-            int newFact = 3;                  
+            //////////////////////////////////////////////////////////////////////
+            /// The prime factors of 13195 are 5, 7, 13 and 29.                ///
+            ///                                                                ///
+            /// What is the largest prime factor of the number 600851475143?   ///
+            ///                                                                ///
+            /// Written by Jessica Luk-Maduro on 29-11-2018                    ///
+            //////////////////////////////////////////////////////////////////////
 
-            while (currentNum > 1)
-            {
-                // any prime factor cannot be even
-                if (newFact%2 == 0)
-                {
-                    newFact++;
-                }
+            long number = 600851475143;
+            long primeInNum = 0;
 
-                else if (currentNum%newFact == 0)
-                {
-                    currentNum = currentNum/newFact;
 
-                    if(newFact>largestFact)
-                    {
-                        largestFact = newFact;
-                    }
-                }
-                
-            }
 
-            Console.WriteLine("The largest prime factor of " 
-                + number + " is " + largestFact + ".");
-            Console.ReadLine();
         }
     }
 }
